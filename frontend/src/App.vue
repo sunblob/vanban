4e15480ceac0aad5e28e98cefcea1062ef7d60dd
sunblob <sunrisehype@gmail.com>
2024-01-11 21:47:47 +0100

    started making header component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import VanbanSidebar from './components/vanban-sidebar.vue';
+import TheHeader from './components/common/the-header.vue';
 </script>
 
 <template>
+  <the-header />
+
   <router-view />
 </template>
---
+++ frontend/src/components/common/the-header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
+    <div class="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+      Vanban
+      <div class="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
+        <v-button size="sm">
+          <router-link to="/sign-in"> Login </router-link>
+        </v-button>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { RouterLink } from 'vue-router';
+import VButton from '../ui/v-button.vue';
+
+export default defineComponent({
+  components: {
+    VButton,
+    RouterLink,
+  },
+});
+</script>
+
+<style scoped></style>
